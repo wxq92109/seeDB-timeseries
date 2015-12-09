da64289add92ced26bb38b8431d03f3482af8d9e
Deborah <deborahc@mit.edu>
2015-12-08 19:57:28 -0500

    formatting changes to server

--- seeDB-timeseries/src/timeseries/Server.java
+++ seeDB-timeseries/src/timeseries/Server.java
@@ -23,65 +23,70 @@ import com.sun.net.httpserver.Headers;
 
 public class Server {
 
-	  @SuppressWarnings("restriction")
+	@SuppressWarnings("restriction")
 	public static void main(String[] args) throws Exception {
-	    @SuppressWarnings("restriction")
+		@SuppressWarnings("restriction")
 		HttpServer server = HttpServer.create(new InetSocketAddress(8081), 0);
-	    server.createContext("/getResult", new ResultHandler());
-	    server.setExecutor(null); // creates a default executor
-	    server.start();
-	  }
-	  
+		server.createContext("/getResult", new ResultHandler());
+		server.setExecutor(null); // creates a default executor
+		server.start();
+	}
 
+	static class ResultHandler implements HttpHandler {
+		@SuppressWarnings("restriction")
+		public void handle(HttpExchange t) throws IOException {
+			// Parse user inputs (hashtag, start time, end time)
+			Map<String, String> params = queryToMap(t.getRequestURI().getQuery());
+			LinkedHashMap<String, HashMap<Timestamp, Double>> results = getResults(params);
+			System.out.println(t.getResponseHeaders());
+			Headers h = t.getResponseHeaders();
+			// these are the user inputs
+			System.out.println(params);
+			h.add("Content-Type", "application/json");
+			Gson gson = new Gson();
+			String json = gson.toJson(results);
+			System.out.println("json" + json);
+			t.sendResponseHeaders(200, json.length());
+			OutputStream os = t.getResponseBody();
+			os.write(json.getBytes());
+			os.close();
+		}
 
-  static class ResultHandler implements HttpHandler {
-    @SuppressWarnings("restriction")
-	public void handle(HttpExchange t) throws IOException {
-    	// step 1: connect to db
-    	
-		DBSetting s = DBSetting.getDefault();
-		
-		SeeDB seedb = new SeeDB();
-		seedb.connectToDatabase(s.database, s.databaseType, s.username, s.password);
-		System.out.println("connected to db");
-		seedb.binTimeData("hashtags", "hashtags_by_hour", Timestamp.valueOf("2015-02-24 00:00:00.0"), Timestamp.valueOf("2015-02-24 05:00:00.0"));
-		String target = "job";
-		String[] candidates = {"jobs", "kca", "tweetmyjobs", "vote1duk"};
-		seedb.computeCorrelation(target, candidates);
-		LinkedHashMap<String, HashMap<Timestamp, Double>> results = (LinkedHashMap<String, HashMap<Timestamp, Double>>) seedb.getHighlyCorrelated(5);
+		// this function is from:
+		// http://www.rgagnon.com/javadetails/java-get-url-parameters-using-jdk-http-server.html
+		private static Map<String, String> queryToMap(String query) {
+			Map<String, String> result = new HashMap<String, String>();
+			for (String param : query.split("&")) {
+				String pair[] = param.split("=");
+				if (pair.length > 1) {
+					result.put(pair[0], pair[1]);
+				} else {
+					result.put(pair[0], "");
+				}
+			}
+			return result;
+		}
 
-		results.forEach((k, v) -> System.out.println(k + "=" + v));
-//		String response = "Response: ";
-//		for (Entry<String, HashMap<Timestamp, Double>> result : results.entrySet()) {
-//			response += result.getKey() + "=" + result.getValue();
-//		}
-		System.out.println(t.getResponseHeaders());
-	    Headers h = t.getResponseHeaders();
-	    // these are the user inputs
-	    Map <String,String>params = queryToMap(t.getRequestURI().getQuery());
-	    System.out.println(params);
-	    h.add("Content-Type", "application/json");
-		Gson gson = new Gson(); 
-		String json = gson.toJson(results); 
-		System.out.println("json" + json);
-		t.sendResponseHeaders(200, json.length());
-		OutputStream os = t.getResponseBody();
-		os.write(json.getBytes());
-		os.close();
-    }
-    
-    // this function is from: http://www.rgagnon.com/javadetails/java-get-url-parameters-using-jdk-http-server.html
-	  public static Map<String, String> queryToMap(String query){
-		    Map<String, String> result = new HashMap<String, String>();
-		    for (String param : query.split("&")) {
-		        String pair[] = param.split("=");
-		        if (pair.length>1) {
-		            result.put(pair[0], pair[1]);
-		        }else{
-		            result.put(pair[0], "");
-		        }
-		    }
-		    return result;
-		  }
-  	}
+		// function to get results from seedb backend
+		private LinkedHashMap<String, HashMap<Timestamp, Double>> getResults(Map<String, String> params) {
+			DBSetting s = DBSetting.getDefault();
+
+			SeeDB seedb = new SeeDB();
+			seedb.connectToDatabase(s.database, s.databaseType, s.username, s.password);
+			System.out.println("connected to db");
+			
+			seedb.binTimeData("hashtags", "hashtags_by_hour", Timestamp.valueOf("2015-02-24 00:00:00.0"),
+			Timestamp.valueOf("2015-02-24 05:00:00.0"));
+			String target = "job";
+			String[] candidates = { "jobs", "kca", "tweetmyjobs", "vote1duk" };
+			seedb.computeCorrelation(target, candidates);
+			LinkedHashMap<String, HashMap<Timestamp, Double>> results = (LinkedHashMap<String, HashMap<Timestamp, Double>>) seedb
+					.getHighlyCorrelated(5);
+//			results.forEach((k, v) -> System.out.println(k + "=" + v));
+
+			return results;
+		}
+
+	}
+
 }
